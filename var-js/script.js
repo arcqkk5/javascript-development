e64f67c2968d8cf9212e8f851f7e62517b09941a
arcq-k5 <arcqk5@yandex.ru>
2021-11-29 23:43:30 +0300

    add var-js

---
+++ var-js/script.js
@@ -0,0 +1,20 @@
+var title = 'Jack';
+console.log(title);
+title = 'Grigoriy'
+console.log(title);
+var color;
+console.log(color);
+color = 'red';
+console.log(color);
+
+/****************************************************
+ * JavaScript variables names can include
+ * letters, numbers, $, _ 
+ * JavaScript variables names can't start with number
+ *****************************************************/
+
+
+myMainColor = 'green'; //camelCase
+my_main_color; //snake_case
+my-main-color; //kebab-case
+MyMainCase // PascalCase
