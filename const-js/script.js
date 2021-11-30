f5af58f5ebc4a40d974447d572315bd7b5d882a3
arcq-k5 <arcqk5@yandex.ru>
2021-11-30 22:45:42 +0300

    add const-js

---
+++ const-js/script.js
@@ -0,0 +1,16 @@
+const man = {
+  name = 'Jack',
+  age = '26',
+  male = 'men'
+}
+
+console.log(man);
+console.log(man.age);
+
+men.male = 'woman'
+
+// Но нельзя:
+
+// man = {
+//   name = 'jane'
+// }
