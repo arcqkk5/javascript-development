8426229bdf979312a63633f4890b203303d691ad
arcq-k5 <arcqk5@yandex.ru>
2021-12-02 00:01:26 +0300

    add js

---
+++ statementsAndExpression-js/script.js
@@ -0,0 +1,17 @@
+// // Expression
+// 2+2
+// 37
+// true && false
+
+// //Statements
+// const number = 10;
+// if(number > 0) {
+//   const text = 'The number is positive'
+// } else {
+//   console.log(number)
+// }
+
+const money = 100;
+const cost = 50;
+money >= cost ? console.log('You can buy it') : console.log('You can not buy it')
+
