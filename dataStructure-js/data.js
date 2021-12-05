d6122737a0727c37b25f54e4057ac3e699365d1d
arcqkk5 <arcq-k5@yandex.ru>
2021-12-05 18:06:42 +0300

    add date

--- dataStructure-js/data.js
+++ dataStructure-js/data.js
@@ -1,41 +1,105 @@
 'use strict';
 
 const japanesRestaurant = {
-  name: 'Banzai',
+  nameF: 'Banzai',
   location: '108 Markham Woods Rd, Longwood, USA',
   categories: ['Japanese', 'Sushi', 'Vegetarian', 'Organic'],
   appetizers: ['Seaweed salad', 'Tempura shrimp', 'Edamame', 'Sushi rice'],
   mainMenu: ['Sushi', 'Ramen', 'Tempura'],
+  workingHours: {
+    wed: {
+      open: 10,
+      close: 23,
+    },
+    fri: {
+      open: 10,
+      close: 23,
+    },
+    sun: {
+      open: 12,
+      close: 22,
+    },
+  },
+
   orderFoot: function (appetizersIndex, mainMenuIndex) {
     return [this.appetizers[appetizersIndex], this.mainMenu[mainMenuIndex]];
   },
 };
 
-//destructure
-const arr = [3, 5, 7];
-const x1 = arr[0];
-const x2 = arr[1];
-const x3 = arr[2];
+/**********************************
+ * DISTRUCTURE ARRAY
+ * *******************************/
+// const arr = [3, 5, 7];
+// const x1 = arr[0];
+// const x2 = arr[1];
+// const x3 = arr[2];
 
-const [y1, y2, y3] = arr;
-console.log(y1, y2, y3);
+// const [y1, y2, y3] = arr;
+// console.log(y1, y2, y3);
 
-let [main, , secondary] = japanesRestaurant.categories;
-console.log(main, secondary);
+// let [main, , secondary] = japanesRestaurant.categories;
+// console.log(main, secondary);
 
-//swap
-[secondary, main] = [main, secondary];
-console.log(main, secondary);
+// //swap
+// [secondary, main] = [main, secondary];
+// console.log(main, secondary);
 
-// console.log(japanesRestaurant.orderFoot(2, 1));
-const [appetizer, mainFood] = japanesRestaurant.orderFoot(2, 1);
-console.log(appetizer, mainFood);
+// // console.log(japanesRestaurant.orderFoot(2, 1));
+// const [appetizer, mainFood] = japanesRestaurant.orderFoot(2, 1);
+// console.log(appetizer, mainFood);
 
-const nestedArr = [1, 2, [4, 5]];
-const [mass1, , [mass4, mass5]] = nestedArr;
-console.log(mass1, mass4, mass5);
+// const nestedArr = [1, 2, [4, 5]];
+// const [mass1, , [mass4, mass5]] = nestedArr;
+// console.log(mass1, mass4, mass5);
 
-const newArr = [3, 5];
+// const newArr = [3, 5];
 
-const [a = 0, b = 0, c = 0] = newArr;
-console.log(a, b, c);
+// const [a = 0, b = 0, c = 0] = newArr;
+// console.log(a, b, c);
+
+/**********************************
+ * DISTRUCTURE OBJECT
+ * *******************************/
+// const { nameF, categories, workingHours } = japanesRestaurant;
+
+// console.log(nameF, categories, workingHours);
+
+// const {
+//   nameF: restName,
+//   categories: cat,
+//   workingHours: www,
+// } = japanesRestaurant;
+// console.log(restName, cat, www);
+
+// let x = 5;
+// let y = 6;
+// const obj = { x: 30, y: 40, z: 50 };
+
+// ({ x, y } = obj);
+// console.log(x, y);
+
+/**************
+ * ****SPREAD**
+ * ************/
+const arr = [1, 3, 5];
+//old aproach
+const newArr = [7, 9, arr[0], arr[1], arr[2]];
+
+//new aproach
+const newArr2 = [7, 9, ...arr];
+console.log(newArr);
+console.log(newArr2);
+console.log(...newArr2);
+
+const newMainMenu = [...japanesRestaurant.mainMenu, 'qwerty', 'ghjj'];
+console.log(newMainMenu);
+
+// Array copy
+const categoriesCopy = [...japanesRestaurant.categories];
+
+//Merge arrays
+const fullMenu = [
+  ...japanesRestaurant.appetizers,
+  ...japanesRestaurant.mainMenu,
+];
+console.log(fullMenu);
